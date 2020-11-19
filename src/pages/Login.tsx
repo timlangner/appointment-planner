73bb0747f3cc12a6b365b6b29c3471b8f04c3b84
Tim <tim.langner0@gmail.com>
2020-11-19 17:05:50 +0100

    :construction: Add form elements

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import './login.css';
-import { IonButton } from "@ionic/react";
+import {IonButton, IonInput, IonItem, IonLabel} from "@ionic/react";
 
 const Login = () => {
     return (
         <>
             <div className="login-wrapper">
-                <h1>Login with Google</h1>
-                <IonButton color="danger" expand="block">Block Button</IonButton>
+                <div className="login-inputs">
+                    <IonItem>
+                        <IonLabel position="floating">E-Mail</IonLabel>
+                        <IonInput type="email" inputmode="email" required/>
+                    </IonItem>
+                    <IonItem>
+                        <IonLabel position="floating">Passwort</IonLabel>
+                        <IonInput type="password" required/>
+                    </IonItem>
+                    <IonButton color="primary" expand="block" className="submit-button">Sign In</IonButton>
+                </div>
             </div>
         </>
     );
